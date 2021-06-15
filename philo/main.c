2c32ebacf3754e3924e22890569ddb801f201ef4
Abdelhakim Nassif <anassif@e2r1p7.1337.ma>
2021-06-15 20:12:50 +0100

    norming

--- philo/main.c
+++ philo/main.c
@@ -6,58 +6,11 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 16:05:34 by anassif           #+#    #+#             */
-/*   Updated: 2021/06/15 19:47:32 by anassif          ###   ########.fr       */
+/*   Updated: 2021/06/15 20:11:01 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo1.h"
-
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	ft_atoi(const char *str)
-{
-	long long	n;
-	int			sign;
-	int			i;
-
-	i = 0;
-	sign = 1;
-	n = 0;
-	while (str[i] != '\0' && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i++] == '-')
-			sign = -1;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		n = n * 10;
-		n = n + str[i] - '0';
-		i++;
-	}
-	if (n < 0 && sign > 0)
-		return (-1);
-	if (n < 0 && sign < 0)
-		return (0);
-	return (n * sign);
-}
-
-int	fail(char *str)
-{
-	write(1, str, ft_strlen(str));
-	return (1);
-}
 
 int	check_args(char **av)
 {
---
+++ philo/philo.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/15 20:08:21 by anassif           #+#    #+#             */
+/*   Updated: 2021/06/15 20:12:20 by anassif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# define START 0
+# define EAT 1
+# define SLEEP 2
+# define THINKING 3
+# define DEAD 4
+# define LEFT_FORK 5
+# define RIGHT_FORK 6
+
+typedef struct s_arg
+{
+	int					all_eat;
+	int					number;
+	unsigned long long	time_todie;
+	unsigned long long	time_toeat;
+	unsigned long long	time_tosleep;
+	int					must_eat;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		protect_output;
+}				t_arg;
+
+typedef struct s_philo
+{
+	int					id;
+	unsigned long long	last_eat;
+	int					eat_counter;
+	pthread_t			t;
+	t_arg				*arg;
+	int					state;
+}				t_philo;
+
+int	ft_strlen(const char *str);
+int	ft_atoi(const char *str);
+int	fail(char *str);
+#endif
