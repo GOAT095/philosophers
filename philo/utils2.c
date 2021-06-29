a6ad2780c490b3e9d1b4e4b704097f6c5e6d25de
Abdelhakim Nassif <anassif@e0r3p10.1337.ma>
2021-06-29 17:25:35 +0100

    ...

--- philo/utils2.c
+++ philo/utils2.c
@@ -6,7 +6,7 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 21:49:21 by anassif           #+#    #+#             */
-/*   Updated: 2021/06/29 14:07:07 by anassif          ###   ########.fr       */
+/*   Updated: 2021/06/29 17:25:03 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,21 @@ void	print_it(t_philo *philo, int i, int x)
 	pthread_mutex_lock(&philo->arg->protect_output);
 	if (i == DEAD)
 	{
-		printf("%llu %d is dead\n",
+		printf("\033[0;31m%llu %d is dead\n",
 			get_time() - philo->arg->program_start, philo[x].id + 1);
-		usleep(10);
 		return ;
 	}
 	else if (i == THINKING && philo->arg->dead == 0)
-		printf("%llu %d is thinking\n",
+		printf("\033[0;37m%llu %d is thinking\n",
 			get_time() - philo->arg->program_start, philo->id + 1);
 	else if (i == EAT && philo->arg->dead == 0)
-		printf("%llu %d is eating\n",
+		printf("\033[0;32m%llu %d is eating\n",
 			get_time() - philo->arg->program_start, philo->id + 1);
 	else if ((i == LEFT_FORK || i == RIGHT_FORK) && philo->arg->dead == 0)
-		printf("%llu %d has taken a fork\n",
+		printf("\033[0;33m%llu %d has taken a fork\n",
 			get_time() - philo->arg->program_start, philo->id + 1);
 	else if (i == SLEEP && philo->arg->dead == 0)
-		printf("%llu %d is sleeping\n",
+		printf("\033[0;35m%llu %d is sleeping\n",
 			get_time() - philo->arg->program_start, philo->id + 1);
 	pthread_mutex_unlock(&philo->arg->protect_output);
 }
@@ -90,9 +89,10 @@ void	*philo_funcn(void *data)
 		pthread_mutex_unlock(&philo->arg->forks[right]);
 		start_sleep = get_time();
 		philo->state = SLEEP;
-		// if (!philo->arg->dead)
-			print_it(philo, SLEEP, 0);
+		print_it(philo, SLEEP, 0);
 		usleep(philo->arg->time_tosleep * 1000);
+		while (get_time() - start_sleep < philo->arg->time_todie)
+		;
 		print_it(philo, THINKING, 0);
 	}
 	return (NULL);
